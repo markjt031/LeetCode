c24008ff5cf624076180d1d79fe623173991635c
Jessica Marks <118943856+markjt031@users.noreply.github.com>
2023-04-25 22:17:00 -0400

    Time: 158 ms (5.16%), Space: 51.5 MB (5.13%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    let sum=0;
+    let badSums=[]
+    while(n>0){
+        sum+=(n%10)**2;
+        n=Math.floor(n/10);
+        
+        if (n===0 && sum!==1){
+            if (badSums.includes(sum)){
+                return false
+            }
+            else {
+                badSums.push(sum)
+                console.log(badSums)
+                n=sum;
+                sum=0
+            }
+        }
+        if (n===0 && sum===1){
+            return true;
+        }
+        
+    }
+};
