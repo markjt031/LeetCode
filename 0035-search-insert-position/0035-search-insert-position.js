64e3462767480f79c2f5c90c02dd487343c77745
Jessica Marks <118943856+markjt031@users.noreply.github.com>
2023-04-29 09:03:34 -0400

    Time: 55 ms (77.10%), Space: 41.9 MB (73.45%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,13 +4,10 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let inc=0
     let start=0
     let end=nums.length-1
-    let middle=Math.floor((start+end)/2)
     while(start<=end){
-        middle=Math.floor((start+end)/2)
-        console.log(middle)
+        let middle=Math.floor((start+end)/2)
         if (target<nums[middle]){
             start=start
             end=middle-1;
@@ -28,8 +25,6 @@ var searchInsert = function(nums, target) {
         else if (start>end && nums[middle]>target){
             return middle
         }
-        console.log("start",start);
-        console.log("end", end)
     }
     
 };
