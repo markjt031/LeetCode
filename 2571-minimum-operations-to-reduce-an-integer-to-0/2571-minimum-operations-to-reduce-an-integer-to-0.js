aa11bd953161c30b7817281918a2904431b434ed
Jessica Marks <118943856+markjt031@users.noreply.github.com>
2023-05-04 05:56:12 -0400

    Time: 67 ms (16.13%), Space: 42.7 MB (22.58%) - LeetHub

--- 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.js
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.js
@@ -6,5 +6,7 @@ var minOperations = function(n) {
     if (Math.log2(n)%1===0){
         return 1;
     }
-    return (Math.min(minOperations(2**(Math.ceil(Math.log2(n)))-n), minOperations(n-2**(Math.floor(Math.log2(n))))))+1;
+    let smallPower=Math.floor(Math.log2(n));
+    let largePower=Math.ceil(Math.log2(n));
+    return (Math.min(minOperations(2**largePower-n), minOperations(n-2**smallPower)))+1;
 };
