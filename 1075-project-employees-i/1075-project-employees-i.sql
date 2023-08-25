b4b0fd3915868701b48e68695c0e0c48380d7ec2
Jessica Marks <118943856+markjt031@users.noreply.github.com>
2023-08-25 13:59:53 -0400

    Time: 4783 ms (30.62%), Space: 0B (100.00%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 SELECT p.project_id, 
 ROUND(AVG(CAST(experience_years AS DECIMAL(5,2))),2) AS average_years
-FROM Project p JOIN Employee e ON p.employee_id=e.employee_id
+FROM Project p INNER JOIN Employee e ON p.employee_id=e.employee_id
 GROUP BY p.project_id
